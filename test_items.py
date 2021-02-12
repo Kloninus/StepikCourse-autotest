9b4d99671b5b9c2412150652f316ff90d4fa9d9b
Pyankov Dmitry <kloninus@gmail.com>
2021-02-12 19:29:36 +0300

    Первая версия

---
+++ test_items.py
@@ -0,0 +1,16 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def is_element_present(browser):
+    try:
+        browser.implicitly_wait(5)
+        browser.find_element_by_css_selector(".btn-add-to-basket")
+        return True
+    except:
+        return False
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    assert is_element_present(browser)==True, "Кнопка добавить в корзину не найдена"
+    time.sleep(10)
